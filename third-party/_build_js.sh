a8c9c790af7d846eb2258a7464c79a0650ef6a99
hu.hua <bocelli.hu@gmail.com>
2013-02-15 14:19:02 +0900

    fix

--- third-party/_build_js.sh
+++ third-party/_build_js.sh
@@ -5,5 +5,5 @@ cd js/src/ &&
 export CFLAGS="-DJS_C_STRINGS_ARE_UTF8"
 make -f Makefile.ref BUILD_OPT=1 && 
 make -f Makefile.ref BUILD_OPT=1 export &&
-mv ../../dist/lib64/* ../../dist/lib/ &&
 cp *.h ../../dist/include/
+mv ../../dist/lib64/* ../../dist/lib/ &&
