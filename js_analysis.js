6b6b8e6e2f9c05f9546eb0657c51d3d2228dda33
bocelli.hu <bocelli.hu@gmail.com>
2013-07-02 18:34:21 +0900

    fix

--- js_analysis.js
+++ js_analysis.js
@@ -1,15 +1,10 @@
 /**
- * @fileoverview Helpers made available via require('js_analysis') once package is
+ * Helpers made available via require('js_analysis') once package is
  * installed.
  */
 
 var path = require('path')
 
-
-/**
- * Where the phantom binary can be found.
- * @type {string}
- */
 exports.path = process.platform === 'win32' ?
     path.join(__dirname, 'release', 'js_analysis.exe') :
     path.join(__dirname, 'release' ,'js_analysis')
